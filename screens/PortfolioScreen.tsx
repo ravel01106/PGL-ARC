14f85094c815ccbc7e16cdff2f27dd82196100d6
ravel01106 <amandaravelocabrera@gmail.com>
2023-12-03 14:12:09 +0000

    feat: created the tab navigator in the PortfolioScreen component

--- screens/PortfolioScreen.tsx
+++ screens/PortfolioScreen.tsx
@@ -1,15 +1,62 @@
 import { StyleSheet, Text, View } from "react-native";
 import React from "react";
-//import appColors from "../assets/styles/appColors";
+import { NavigationContainer } from "@react-navigation/native";
+import {
+  BottomTabNavigationOptions,
+  createBottomTabNavigator,
+} from "@react-navigation/bottom-tabs";
+import CardScreen from "./CardScreen";
+import QrScreen from "./QrScreen";
+import { AntDesign } from "@expo/vector-icons";
+
+import appColors from "../assets/styles/appColors";
+
+const Tab = createBottomTabNavigator();
 
 const PortfolioScreen = () => {
+  const tabNavigationOptions: BottomTabNavigationOptions = {
+    headerShown: false,
+    tabBarInactiveTintColor: "#AAA",
+    tabBarActiveTintColor: appColors.letterWhite,
+    tabBarShowLabel: false,
+    tabBarStyle: {
+      backgroundColor: appColors.primary,
+      height: "9%",
+    },
+  };
+  const CardScreenOptions = (): BottomTabNavigationOptions => {
+    return {
+      tabBarIcon: ({ color }) => (
+        <AntDesign name="idcard" size={42} color={color} />
+      ),
+    };
+  };
+
+  const QrScreenOptions = (): BottomTabNavigationOptions => {
+    return {
+      tabBarIcon: ({ color }) => (
+        <AntDesign name="qrcode" size={42} color={color} />
+      ),
+    };
+  };
   return (
-    <View>
-      <Text>Proximamente en cines :D...</Text>
+    <View style={styles.bodyStyle}>
+      <Tab.Navigator screenOptions={tabNavigationOptions}>
+        <Tab.Screen
+          name="Card"
+          component={CardScreen}
+          options={CardScreenOptions}
+        />
+        <Tab.Screen name="Qr" component={QrScreen} options={QrScreenOptions} />
+      </Tab.Navigator>
     </View>
   );
 };
 
 export default PortfolioScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  bodyStyle: {
+    flex: 20,
+  },
+});
---
+++ screens/QrScreen.tsx
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+
+const QrScreen = () => {
+  return (
+    <View>
+      <Text>QrScreen</Text>
+    </View>
+  );
+};
+
+export default QrScreen;
+
+const styles = StyleSheet.create({});
