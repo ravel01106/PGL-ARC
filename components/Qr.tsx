0195410a60dd45466f022382f8b184f4b1193252
ravel01106 <amandaravelocabrera@gmail.com>
2023-12-04 08:28:37 +0000

    refactor: created Qr component

---
+++ components/Qr.tsx
@@ -0,0 +1,46 @@
+import { StyleSheet, Text, View } from "react-native";
+import React from "react";
+import appColors from "../assets/styles/appColors";
+import { cardInfoContext } from "../context/CardContext";
+import QRCode from "react-native-qrcode-svg";
+
+const Qr = () => {
+  const { title, qr } = React.useContext(cardInfoContext);
+  return (
+    <View style={styles.infoContainer}>
+      <Text style={[styles.text, styles.cardTitle]}>{title}</Text>
+      <View style={[styles.infoContainer, styles.QrContainerStyle]}>
+        <View style={styles.QrStyle}>
+          <QRCode value={qr} size={200} />
+        </View>
+      </View>
+    </View>
+  );
+};
+
+export default Qr;
+
+const styles = StyleSheet.create({
+  infoContainer: {
+    marginVertical: "7%",
+  },
+  text: {
+    fontSize: 19,
+    color: appColors.letter,
+    paddingHorizontal: "5%",
+    paddingVertical: "3%",
+    textAlign: "center",
+  },
+  cardTitle: {
+    fontSize: 26,
+    fontWeight: "bold",
+  },
+  QrContainerStyle: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  QrStyle: {
+    borderWidth: 8,
+    borderColor: "white",
+  },
+});
--- screens/QrScreen.tsx
+++ screens/QrScreen.tsx
@@ -2,23 +2,14 @@ import { StyleSheet, Text, View } from "react-native";
 import React from "react";
 import appColors from "../assets/styles/appColors";
 import FrontCard from "../components/FrontCard";
-import { cardInfoContext } from "../context/CardContext";
-import QRCode from "react-native-qrcode-svg";
+import Qr from "../components/Qr";
 
 const QrScreen = () => {
-  const { title, qr } = React.useContext(cardInfoContext);
   return (
     <View style={styles.mainContainer}>
       <View style={styles.cardContainer}>
         <FrontCard />
-        <View style={styles.infoContainer}>
-          <Text style={[styles.text, styles.cardTitle]}>{title}</Text>
-          <View style={[styles.infoContainer, styles.QrContainerStyle]}>
-            <View style={styles.QrStyle}>
-              <QRCode value={qr} size={200} />
-            </View>
-          </View>
-        </View>
+        <Qr />
       </View>
     </View>
   );
@@ -37,27 +28,5 @@ const styles = StyleSheet.create({
     height: 550,
     backgroundColor: appColors.secundary,
     borderRadius: 20,
-  },
-  infoContainer: {
-    marginVertical: "7%",
-  },
-  text: {
-    fontSize: 19,
-    color: appColors.letter,
-    paddingHorizontal: "5%",
-    paddingVertical: "3%",
-    textAlign: "center",
-  },
-  cardTitle: {
-    fontSize: 26,
-    fontWeight: "bold",
-  },
-  QrContainerStyle: {
-    justifyContent: "center",
-    alignItems: "center",
-  },
-  QrStyle: {
-    borderWidth: 8,
-    borderColor: "white",
   },
 });
