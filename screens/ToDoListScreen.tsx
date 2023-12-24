790be3774db760e7ce435162bc4b09c24e36d531
ravel01106 <amandaravelocabrera@gmail.com>
2023-12-24 19:56:30 +0000

    feat(ToDoListScreen): Service data collected and correctly displayed on the screen

--- screens/ToDoListScreen.tsx
+++ screens/ToDoListScreen.tsx
@@ -1,16 +1,55 @@
-import { Pressable, StyleSheet, Text, View } from "react-native";
+import {
+  Pressable,
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+} from "react-native";
 import React from "react";
 import appColors from "../assets/styles/appColors";
 import { getActivityRandom } from "../services/ActivityRandomService";
+import { FlatList } from "react-native-gesture-handler";
+import { Entypo } from "@expo/vector-icons";
+
+type ActivityData = {
+  index: number;
+  name: string;
+  participants: number;
+  price: number;
+  accessibility: number;
+};
 
 const ToDoListScreen = () => {
+  const [activities, setActivities] = React.useState<ActivityData[]>([]);
+
   const fetchActivityApi = () => {
     const fetchData = async () => {
+      const arrayData: ActivityData[] = [...activities];
+      const index: number = activities.length + 1;
       const data = await getActivityRandom();
-      console.log(data);
+
+      const activity: ActivityData = {
+        index: index,
+        name: data.activity,
+        participants: data.participants,
+        price: data.price,
+        accessibility: data.accessibility,
+      };
+      arrayData.push(activity);
+      setActivities(arrayData);
+      console.log(activities);
     };
+
     fetchData();
   };
+
+  const getActivities = () => {
+    if (activities && activities.length != 0) {
+      return activities;
+    }
+    return [];
+  };
+
   return (
     <View style={styles.mainContainer}>
       <View style={styles.titleMainContainer}>
@@ -20,14 +59,36 @@ const ToDoListScreen = () => {
         </View>
         <View style={[styles.styleFigure, styles.firstFigure]}></View>
       </View>
-      <Pressable onPress={fetchActivityApi} style={styles.buttonContainer}>
+      <Pressable
+        onPress={() => fetchActivityApi()}
+        style={styles.buttonContainer}
+      >
         <Text style={styles.buttonText}>Search</Text>
       </Pressable>
-
       <View style={styles.boxMainContainer}>
-        <View style={styles.subcontainer}>
-          <Text style={styles.styleText}>Waiting to look for activities</Text>
-        </View>
+        <FlatList
+          style={styles.listActivitiesContainer}
+          data={activities}
+          renderItem={(activity) => (
+            <TouchableOpacity
+              style={styles.activityContainer}
+              onPress={() => alert("Yeah boyy")}
+            >
+              <Text style={styles.activityText}>{activity.item.name}</Text>
+              <TouchableOpacity onPress={() => alert("yeah boy")}>
+                <Entypo name="cross" size={34} color={appColors.letterWhite} />
+              </TouchableOpacity>
+            </TouchableOpacity>
+          )}
+          keyExtractor={(activity) => activity.index.toString()}
+          ListEmptyComponent={() => (
+            <View style={styles.subcontainer}>
+              <Text style={styles.styleText}>
+                Waiting to look for activities
+              </Text>
+            </View>
+          )}
+        />
       </View>
     </View>
   );
@@ -89,16 +150,16 @@ const styles = StyleSheet.create({
     height: 450,
     width: "100%",
     backgroundColor: appColors.primary,
-    justifyContent: "center",
-    alignItems: "center",
     borderTopRightRadius: 70,
     borderTopLeftRadius: 70,
   },
   subcontainer: {
-    height: "70%",
-    width: "70%",
+    height: 300,
+    width: "75%",
     backgroundColor: appColors.ternary,
     borderRadius: 70,
+    alignSelf: "center",
+    marginVertical: "20%",
     justifyContent: "space-evenly",
     alignItems: "center",
   },
@@ -116,5 +177,29 @@ const styles = StyleSheet.create({
   },
   styleTextButton: {
     color: appColors.letter,
+  },
+  listActivitiesContainer: {
+    marginTop: 20,
+    marginBottom: 10,
+  },
+  activityContainer: {
+    width: "80%",
+    height: 70,
+    backgroundColor: appColors.ternary,
+    marginVertical: 12,
+    paddingHorizontal: 12,
+    borderRadius: 50,
+    alignSelf: "center",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  activityText: {
+    textAlign: "center",
+    color: appColors.letterWhite,
+    paddingHorizontal: 10,
+    fontSize: 18,
+    flex: 1,
   },
 });
